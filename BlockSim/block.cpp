905982254fb88274514596d3e1d8ea129e667d3a
The-Huginn <rbudinsk@redhat.com>
2022-10-31 17:41:09 +0100

    Adjust block to not contain base block reward.

--- BlockSim/block.cpp
+++ BlockSim/block.cpp
@@ -5,6 +5,9 @@
 //  Created by Harry Kalodner on 5/25/16.
 //  Copyright © 2016 Harry Kalodner. All rights reserved.
 //
+//  Edited by Rastislav Budinsky on 8/31/22.
+//  Copyright © 2022 Rastislav Budinsky. All rights reserved.
+//
 
 #include "block.hpp"
 #include "miner.hpp"
@@ -15,28 +18,32 @@
 
 constexpr auto timeMax = std::numeric_limits<TimeType>::max();
 
-Block::Block(BlockValue blockReward_) : Block(nullptr, nullptr, BlockTime(0), Value(0), BlockHeight(0), Value(0), Value(0), Value(rawValue(blockReward_))) {}
+// Block::Block(BlockValue blockReward_) : Block(nullptr, nullptr, BlockTime(0), Value(0), BlockHeight(0), Value(0), Value(0), Value(rawValue(blockReward_))) {}
 
-Block::Block(const Block *parent_, const Miner *miner_, BlockTime timeSeconds_, Value txFees, BlockHeight height_, Value txFeesInChain_, Value valueInChain_, Value blockReward_) : timeBroadcast(timeMax), parent(parent_), miner(miner_), height(height_), timeMined(timeSeconds_), value(txFees + blockReward_), txFeesInChain(txFeesInChain_), valueInChain(valueInChain_), blockReward(blockReward_) {}
+// Block::Block(const Block *parent_, const Miner *miner_, BlockTime timeSeconds_, Value txFees, BlockHeight height_, Value txFeesInChain_, Value valueInChain_, Value blockReward_) : timeBroadcast(timeMax), parent(parent_), miner(miner_), height(height_), timeMined(timeSeconds_), value(txFees + blockReward_), txFeesInChain(txFeesInChain_), valueInChain(valueInChain_), blockReward(blockReward_) {}
+Block::Block(const Block *parent_, const Miner *miner_, BlockTime timeSeconds_, Value txFees, BlockHeight height_, Value txFeesInChain_, Value valueInChain_) : timeBroadcast(timeMax), parent(parent_), miner(miner_), height(height_), timeMined(timeSeconds_), value(txFees), txFeesInChain(txFeesInChain_), valueInChain(valueInChain_) {}
 
 Block::Block(const Block *parent_, const Miner *miner_, BlockTime timeSeconds_, Value txFees) :
-    Block(parent_, miner_, timeSeconds_, txFees, parent_->height + BlockHeight(1), parent_->txFeesInChain + txFees, parent_->valueInChain + parent_->blockReward + txFees, parent_->nextBlockReward()) {}
+    // Block(parent_, miner_, timeSeconds_, txFees, parent_->height + BlockHeight(1), parent_->txFeesInChain + txFees, parent_->valueInChain + parent_->blockReward + txFees, parent_->nextBlockReward()) {}
+    Block(parent_, miner_, timeSeconds_, txFees, parent_->height + BlockHeight(1), parent_->txFeesInChain + txFees, parent_->valueInChain + txFees) {}
 
 void Block::reset(const Block *parent_, const Miner *miner_, BlockTime timeSeconds_, Value txFees) {
     height = parent_->height + BlockHeight(1);
     timeMined = timeSeconds_;
     timeBroadcast = timeMax;
-    value = txFees + parent_->nextBlockReward();
+    // value = txFees + parent_->nextBlockReward();
+    value = txFees;
     txFeesInChain = txFees + parent_->txFeesInChain;
-    valueInChain = txFees + parent_->valueInChain + parent_->nextBlockReward();
-    blockReward = parent_->nextBlockReward();
+    // valueInChain = txFees + parent_->valueInChain + parent_->nextBlockReward();
+    valueInChain = parent->valueInChain + txFees;
+    // blockReward = parent_->nextBlockReward();
     parent = parent_;
     miner = miner_;
 }
 
-Value Block::nextBlockReward() const {
-    return blockReward;
-}
+// Value Block::nextBlockReward() const {
+//     return blockReward;
+// }
 
 void Block::broadcast(BlockTime timePub) {
     timeBroadcast = timePub;
